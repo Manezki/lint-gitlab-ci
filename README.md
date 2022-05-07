17d982bac401a42f67ef23e6eab9882def8263ba
Janne Holopainen <manezki@gmail.com>
2022-05-07 16:09:45 +0300

    Feat: Parse script sections and pass to shellcheck

---
+++ README.md
@@ -0,0 +1,13 @@
+# Gitlab CI script linter
+
+Lint the script sections of Gitlab CI configurations (.gitlab-ci.yml) 
+
+## Dependencies
+
+* [**shellcheck**](https://github.com/koalaman/shellcheck#installing) needs to be installed in the system
+
+## Running
+
+```bash
+go run main.go PATH_TO_GITLAB_CI
+```
---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Job struct {
+	BeforeScipt []string `yaml:"before_script"`
+	Script      []string `yaml:"script"`
+	AfterScipt  []string `yaml:"after_script"`
+	Image       string   `yaml:"image"`
+	Stage       string   `yaml:"stage"`
+}
+
+func (j Job) completeScript() string {
+	builder := strings.Builder{}
+	for _, l := range j.BeforeScipt {
+		builder.Write([]byte(l))
+		builder.Write([]byte("\n"))
+	}
+	for _, l := range j.Script {
+		builder.Write([]byte(l))
+		builder.Write([]byte("\n"))
+	}
+	for _, l := range j.AfterScipt {
+		builder.Write([]byte(l))
+		builder.Write([]byte("\n"))
+	}
+	return builder.String()
+}
+
+func (j Job) inferShell() string {
+	switch strings.ToLower(j.Image) {
+	case "docker":
+		return "sh"
+	case "python":
+		return "bash"
+	case "":
+		return "bash"
+	default:
+		fmt.Printf("Cannot infer shell type for '%s'. Defaulting to 'bash'\n", j.Image)
+		return "bash"
+	}
+}
+
+func main() {
+	content, _ := os.ReadFile(os.Args[1])
+
+	jobs := make(map[string]Job, 1)
+
+	err := yaml.Unmarshal(content, &jobs)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+	}
+
+	dir, err := os.MkdirTemp(os.TempDir(), "sh-check")
+	if err != nil {
+		fmt.Printf("%s\n", err)
+	}
+	defer os.RemoveAll(dir)
+
+	for name, job := range jobs {
+		tmpFile := path.Join(dir, name)
+		fp, err := os.Create(tmpFile)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			os.Exit(1)
+		}
+		defer fp.Close()
+
+		fp.Write([]byte(job.completeScript()))
+
+		cmd := exec.Command("shellcheck", fmt.Sprintf("--shell=%s", job.inferShell()), tmpFile)
+		out, err := cmd.Output()
+		if err != nil {
+			fmt.Print("===============================================\n")
+			fmt.Printf("%s\n", out)
+		}
+	}
+}
